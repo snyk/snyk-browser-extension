d2d55f896be55c10c8412125ba69c0bd22c7d4d1
Liran Tal <liran.tal@gmail.com>
2023-07-16 11:54:24 +0300

    feat: add critical severity support

--- source/scripts/popup.js
+++ source/scripts/popup.js
@@ -7,15 +7,14 @@ function openWebPage(url) {
 }
 
 function sortByServerity(data) {
-
   const severityMap = {
-    'critical': 4,
-    'high': 3,
-    'medium': 2,
-    'low': 1
+    critical: 4,
+    high: 3,
+    medium: 2,
+    low: 1
   }
 
-  data.sort(function(vuln1, vuln2) {
+  data.sort(function (vuln1, vuln2) {
     var severityVuln1 = severityMap[vuln1.severity.toLowerCase()]
     var severityVuln2 = severityMap[vuln2.severity.toLowerCase()]
     return severityVuln2 - severityVuln1
@@ -24,9 +23,7 @@ function sortByServerity(data) {
   return data
 }
 
-
 function drawVulnerabilities(vulnerabilities) {
-
   const vulnerabilitiesList = document.getElementById('vulnerabilitiesList')
 
   let vulnerabilitiesCards = ''
@@ -35,16 +32,15 @@ function drawVulnerabilities(vulnerabilities) {
   let upgradeInfo = ''
   let directOrIndirect = ''
 
-  vulnerabilitiesCards += `<p class="number">We've found ${vulnerabilities.length} vulnerabilities</p>`;
+  vulnerabilitiesCards += `<p class="number">We've found ${vulnerabilities.length} vulnerabilities</p>`
 
   const vulnerabilitiesSorted = sortByServerity(vulnerabilities)
-  vulnerabilitiesSorted.forEach(vulnerability => {
-
+  vulnerabilitiesSorted.forEach((vulnerability) => {
     let severityClass = ''
     let severityText = ''
     switch (vulnerability.severity) {
-      case 'critical': 
-      case 'high': 
+      case 'critical':
+      case 'high':
         severityClass = 'high'
         severityText = 'H'
         break
@@ -121,11 +117,19 @@ function drawVulnerabilities(vulnerabilities) {
 
           <div class="d-flex justify-content-between border-top pt-2">
             <dt class="font-weight-normal">CVEs</dt>
-            <dd class="my-0 number">${vulnerability.identifiers.CVE.length > 0 ? vulnerability.identifiers.CVE.join(' ') : '–'}</dd>
+            <dd class="my-0 number">${
+              vulnerability.identifiers.CVE.length > 0
+                ? vulnerability.identifiers.CVE.join(' ')
+                : '–'
+            }</dd>
           </div>
           <div class="d-flex justify-content-between border-top pt-2">
             <dt class="font-weight-normal">CWEs</dt>
-            <dd class="my-0 number">${vulnerability.identifiers.CWE.length > 0 ? vulnerability.identifiers.CWE.join(' ') : '–'}</dd>
+            <dd class="my-0 number">${
+              vulnerability.identifiers.CWE.length > 0
+                ? vulnerability.identifiers.CWE.join(' ')
+                : '–'
+            }</dd>
           </div>
           <div class="d-flex justify-content-between border-top pt-2">
             <dt class="font-weight-normal">Disclosed at</dt>
@@ -134,49 +138,51 @@ function drawVulnerabilities(vulnerabilities) {
 
         </dl>
 
-        <a target="_blank" href="${vulnerability.url}?utm_medium=Referral&utm_source=Google&utm_campaign=Chrome-Extension&utm_content=Vuln" onClick="browser.tabs.create({"url": "${vulnerability.url}?utm_medium=Referral&utm_source=Google&utm_campaign=Chrome-Extension&utm_content=Vuln"})">Learn more</a>
+        <a target="_blank" href="${
+          vulnerability.url
+        }?utm_medium=Referral&utm_source=Google&utm_campaign=Chrome-Extension&utm_content=Vuln" onClick="browser.tabs.create({"url": "${
+      vulnerability.url
+    }?utm_medium=Referral&utm_source=Google&utm_campaign=Chrome-Extension&utm_content=Vuln"})">Learn more</a>
 
       </div>
     </div>
     `
   })
 
-  vulnerabilitiesList.innerHTML = vulnerabilitiesCards;
+  vulnerabilitiesList.innerHTML = vulnerabilitiesCards
 
-  $('#loader').addClass('d-none');
-
+  $('#loader').addClass('d-none')
 }
 
-
-async function retrieveSnykInformation({snykApiToken}) {
-
+async function retrieveSnykInformation({ snykApiToken }) {
   let response
   try {
-    const tabs = await browser.tabs.query({active: true, currentWindow: true})
-    response = await browser.tabs.sendMessage(tabs[0].id, {greeting: "hello"})
+    const tabs = await browser.tabs.query({ active: true, currentWindow: true })
+    response = await browser.tabs.sendMessage(tabs[0].id, { greeting: 'hello' })
   } catch (error) {
-    $('#results').addClass('d-none');
-    $('#errorPackageDetails').removeClass('d-none');
-    $('details').attr('open', false).find('summary').removeClass('d-none');
-    return;
+    $('#results').addClass('d-none')
+    $('#errorPackageDetails').removeClass('d-none')
+    $('details').attr('open', false).find('summary').removeClass('d-none')
+    return
   }
 
   if (!response) {
-    $('#results').addClass('d-none');
-    $('#errorPackageDetails').removeClass('d-none');
-    $('details').attr('open', false).find('summary').removeClass('d-none');
+    $('#results').addClass('d-none')
+    $('#errorPackageDetails').removeClass('d-none')
+    $('details').attr('open', false).find('summary').removeClass('d-none')
   }
 
   if (snykApiToken && response.packageName && response.packageVersion) {
-    
     document.getElementById('packageName').textContent = response.packageName
     document.getElementById('packageVersion').textContent = `v${response.packageVersion}`
 
-    $('#loader').removeClass('d-none');
+    $('#loader').removeClass('d-none')
 
-    const advisorUrl = `https://snyk.io/advisor/npm-package/${response.packageName}?utm_medium=Referral&utm_source=Google&utm_campaign=Chrome-Extension&utm_content=Advisor`;
-    const scoreUrl = `http://snyk.io/advisor/npm-package/${response.packageName}/score`;
-    $('#healthscore').after(`<a target="_blank" href="${advisorUrl}" onClick="browser.tabs.create({"url": "${advisorUrl}"})" class="d-block px-3 pb-3 pt-0">View package health on Snyk Advisor</a>`);
+    const advisorUrl = `https://snyk.io/advisor/npm-package/${response.packageName}?utm_medium=Referral&utm_source=Google&utm_campaign=Chrome-Extension&utm_content=Advisor`
+    const scoreUrl = `http://snyk.io/advisor/npm-package/${response.packageName}/score`
+    $('#healthscore').after(
+      `<a target="_blank" href="${advisorUrl}" onClick="browser.tabs.create({"url": "${advisorUrl}"})" class="d-block px-3 pb-3 pt-0">View package health on Snyk Advisor</a>`
+    )
     $('#healthscore iframe').prop('src', scoreUrl)
 
     const url = `https://snyk.io/api/v1/test/npm/${response.packageName}/${response.packageVersion}`
@@ -184,125 +190,142 @@ async function retrieveSnykInformation({snykApiToken}) {
       const vulnerabilitiesData = await fetch(url, {
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `token ${snykApiToken}`
+          Authorization: `token ${snykApiToken}`
         }
       })
 
       // authentication error
       if (vulnerabilitiesData.status === 401) {
+        $('#results').addClass('d-none')
+        $('#vulnerabilitiesList').addClass('d-none')
+        $('#loader').addClass('d-none')
+        $('#errorAPITokenMissing').removeClass('d-none')
+        $('details')
+          .attr('open', true)
+          .find('summary')
+          .addClass('d-none')
+          .end()
+          .find('input')
+          .focus()
 
-        $('#results').addClass('d-none');
-        $('#vulnerabilitiesList').addClass('d-none');
-        $('#loader').addClass('d-none');
-        $('#errorAPITokenMissing').removeClass('d-none');
-        $('details').attr('open', true).find('summary').addClass('d-none').end().find('input').focus();
-      
-      // authentication ok
+        // authentication ok
       } else {
-
-        $('#results').removeClass('d-none');
-        $('#vulnerabilitiesList').removeClass('d-none');
-        $('details').attr('open', false).find('summary').removeClass('d-none');
+        $('#results').removeClass('d-none')
+        $('#vulnerabilitiesList').removeClass('d-none')
+        $('details').attr('open', false).find('summary').removeClass('d-none')
 
         const snykPackageData = await vulnerabilitiesData.json()
         const vulnerabilityIssues = snykPackageData.issues.vulnerabilities
         const licenseIssues = snykPackageData.issues.licenses
-        
-        if (vulnerabilityIssues.length > 0) {
 
-          let high = 0;
-          let medium = 0;
-          let low = 0;
-          
-          vulnerabilityIssues.forEach(function(item, i){
-            if(item.severity === 'high') {
-              ++high;
+        if (vulnerabilityIssues.length > 0) {
+          let critical = 0
+          let high = 0
+          let medium = 0
+          let low = 0
+
+          vulnerabilityIssues.forEach(function (item, i) {
+            if (item.severity === 'critical') {
+              ++critical
             }
-            if(item.severity === 'medium') {
-              ++medium;
+            if (item.severity === 'high') {
+              ++high
             }
-            if(item.severity === 'low') {
-              ++low;
+            if (item.severity === 'medium') {
+              ++medium
+            }
+            if (item.severity === 'low') {
+              ++low
             }
           })
 
-          if(high > 0) {
-            document.getElementById('totalVulnerabilitiesHigh').querySelector('.number').textContent = high
+          if (high > 0) {
+            document
+              .getElementById('totalVulnerabilitiesCritical')
+              .querySelector('.number').textContent = critical
+            document.getElementById('totalVulnerabilitiesCritical').classList.add('critical')
+          }
+
+          if (high > 0) {
+            document
+              .getElementById('totalVulnerabilitiesHigh')
+              .querySelector('.number').textContent = high
             document.getElementById('totalVulnerabilitiesHigh').classList.add('high')
           }
 
-          if(medium > 0) {
-            document.getElementById('totalVulnerabilitiesMedium').querySelector('.number').textContent = medium
+          if (medium > 0) {
+            document
+              .getElementById('totalVulnerabilitiesMedium')
+              .querySelector('.number').textContent = medium
             document.getElementById('totalVulnerabilitiesMedium').classList.add('medium')
           }
 
-          if(low > 0) {
-            document.getElementById('totalVulnerabilitiesLow').querySelector('.number').textContent = low
+          if (low > 0) {
+            document
+              .getElementById('totalVulnerabilitiesLow')
+              .querySelector('.number').textContent = low
             document.getElementById('totalVulnerabilitiesLow').classList.add('low')
           }
-
         }
 
         if (licenseIssues.length > 0) {
+          let high = 0
+          let medium = 0
+          let low = 0
 
-          let high = 0;
-          let medium = 0;
-          let low = 0;
-          
-          vulnerabilityIssues.forEach(function(item, i){
-            if(item.severity === 'high') {
-              ++high;
+          vulnerabilityIssues.forEach(function (item, i) {
+            if (item.severity === 'high') {
+              ++high
             }
-            if(item.severity === 'medium') {
-              ++medium;
+            if (item.severity === 'medium') {
+              ++medium
             }
-            if(item.severity === 'low') {
-              ++low;
+            if (item.severity === 'low') {
+              ++low
             }
           })
 
-          if(high > 0) {
-            document.getElementById('totalLicenseIssuesHigh').querySelector('.number').textContent = high
+          if (high > 0) {
+            document
+              .getElementById('totalLicenseIssuesHigh')
+              .querySelector('.number').textContent = high
             document.getElementById('totalLicenseIssuesHigh').classList.add('high')
           }
 
-          if(medium > 0) {
-            document.getElementById('totalLicenseIssuesMedium').querySelector('.number').textContent = medium
+          if (medium > 0) {
+            document
+              .getElementById('totalLicenseIssuesMedium')
+              .querySelector('.number').textContent = medium
             document.getElementById('totalLicenseIssuesMedium').classList.add('medium')
           }
 
-          if(low > 0) {
-            document.getElementById('totalLicenseIssuesLow').querySelector('.number').textContent = low
+          if (low > 0) {
+            document
+              .getElementById('totalLicenseIssuesLow')
+              .querySelector('.number').textContent = low
             document.getElementById('totalLicenseIssuesLow').classList.add('low')
           }
-
         }
 
         drawVulnerabilities(vulnerabilityIssues)
-
       }
-      
-    } catch (err) {
-      
-    }
+    } catch (err) {}
   }
-
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-
   const storage = await browser.storage.local.get()
   snykApiToken = storage['apiToken']
-  
+
   if (snykApiToken) {
     document.getElementById('snykApiToken').value = snykApiToken
-    await retrieveSnykInformation({snykApiToken})
+    await retrieveSnykInformation({ snykApiToken })
   }
 
   if (!snykApiToken) {
-    $('#results').addClass('d-none');
-    $('#errorAPITokenMissing').removeClass('d-none');
-    $('details').attr('open', true).find('summary').addClass('d-none').end().find('input').focus();
+    $('#results').addClass('d-none')
+    $('#errorAPITokenMissing').removeClass('d-none')
+    $('details').attr('open', true).find('summary').addClass('d-none').end().find('input').focus()
   }
 
   document.getElementById('link_website').addEventListener('click', () => {
@@ -315,18 +338,14 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   const buttonSaveApiToken = document.getElementById('buttonSaveApiToken')
   buttonSaveApiToken.addEventListener('click', async () => {
-    
     const snykApiTokenElement = document.getElementById('snykApiToken')
     const snykApiTokenValue = snykApiTokenElement.value
     await browser.storage.local.set({ apiToken: snykApiTokenValue })
 
     if (snykApiTokenValue) {
       $('#errorAPITokenMissing').toggleClass('d-none')
-      $(buttonSaveApiToken).attr('disabled', false);
-      await retrieveSnykInformation({snykApiToken: snykApiTokenValue})
+      $(buttonSaveApiToken).attr('disabled', false)
+      await retrieveSnykInformation({ snykApiToken: snykApiTokenValue })
     }
-
   })
-
 })
-
